ca4a48b10affcf60113908607fea619fcca65e56
Maikel33 <perezguedesmaikel@gmail.com>
2023-07-01 16:53:03 -0400

    fix image

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -113,7 +113,7 @@ export default function Home() {
                 <section id="section_about" className="h-auto min-h-[40rem] pt-[6.5rem]">
                     <div className="flex flex-col-reverse md:flex-row md:gap-x-8 w-full h-full">
                         <div className="w-full h-[300px] sm:h-[600]  md:w-1/2 relative mt-20 md:mt-20">
-                            <Image src="/images/about.png" alt="AboutUs Image" fill objectFit={"contain"}/>
+                            <Image src="/images/about.png" alt="AboutUs Image" width={600} height={600}/>
                         </div>
                         <div className="w-full md:w-1/2">
                             <PageSectionTitle title="Sobre Nosotros" otherClasses="text-end" />
